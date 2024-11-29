834db2afddbe58442ebd4085d5a3693fc2d45c21
RayTempe <150879391+RayTempe@users.noreply.github.com>
2024-11-29 20:44:37 +0700

    update api login

--- app/src/main/java/com/example/coffeeshopapplication/Interface_API/ApiService.java
+++ app/src/main/java/com/example/coffeeshopapplication/Interface_API/ApiService.java
@@ -1,41 +1,45 @@
 package com.example.coffeeshopapplication.Interface_API;
 
-import android.view.Menu;
-
 import com.example.coffeeshopapplication.Model.MenuResponse;
-import com.example.coffeeshopapplication.Model.PostMenuResponse;
+import com.example.coffeeshopapplication.Model.LoginResponse;
+import com.example.coffeeshopapplication.Model.User;
 import com.example.coffeeshopapplication.Product;
-import com.google.gson.JsonObject;
 
-import java.util.List;
-
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.http.DELETE;
-import retrofit2.http.GET;
-import retrofit2.http.Multipart;
-import retrofit2.http.POST;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.PUT;
-import retrofit2.http.Part;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ApiService {
+    // Endpoint untuk mendapatkan menu
     @GET("MenuApi.php")
     Call<MenuResponse> getMenu();
 
-    @POST("MenuApi.php")
-    Call<ResponseBody> addMenu(@Body JsonObject menuData);
+    // Endpoint untuk menambahkan item ke keranjang
+    @POST("cart/item")
+    Call<Void> addCartItem(@Body Product product);
 
-    @PUT("MenuApi.php/{id}")
+    // Endpoint untuk memperbarui item di keranjang
+    @PUT("cart/item/{id}")
     Call<Void> updateCartItem(@Path("id") int id, @Body Product product);
 
-    @DELETE("MenuApi.php/{id}")
+    // Endpoint untuk menghapus item dari keranjang
+    @DELETE("cart/item/{id}")
     Call<Void> deleteCartItem(@Path("id") int id);
+
+
+    // Endpoint untuk login menggunakan parameter FormUrlEncoded
+    @FormUrlEncoded
+    @POST("EmployeeApi.php")
+    Call<LoginResponse> login(
+            @Query("action") String action, // Query parameter untuk action=login
+            @Field("Email") String email,
+            @Field("Password") String password
+    );
 }
-
-
-
-
